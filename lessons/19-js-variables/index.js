1b7ae951fdc1d75887450f499d40d05740acd485
rachna-jain <rachna.jainin1978@gmail.com>
2019-10-15 19:50:01 -0600

    completed lesson 19

---
+++ lessons/19-js-variables/index.js
@@ -0,0 +1,20 @@
+const monthOfBirth="March";
+const firstName="Rachna";
+const lastName="Jain";
+
+let city;
+let age;
+let nextCity;
+
+city="saskatoon";
+age=55;
+nextCity=city;
+
+console.log("Month of Birth:", monthOfBirth);
+console.log("First Name:", firstName);
+console.log("Last Name:", lastName);
+console.log("Full name:",`${firstName} ${lastName}`);
+console.log("Age:",age);
+console.log("City:",city);
+console.log("Next City:",nextCity);
+
