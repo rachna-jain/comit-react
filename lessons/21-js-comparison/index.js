56f4341cfed5982c52d7f52f170499b842ae8306
rachna-jain <rachna.jainin1978@gmail.com>
2019-10-17 20:01:48 -0600

    completed lesson 21

--- lessons/21-js-comparison/index.js
+++ lessons/21-js-comparison/index.js
@@ -1,7 +1,5 @@
-alert(
-    enter two numbers if th(e first is greater than second number
-);
-const firstNumber= prompt(enter first number);
-const secondNumber=prompt(enter the second number);
-const accurate= confirm(firstNumber>secondNumber);
+alert( "enter two numbers if the first is greater than second number");
+const firstNumber= prompt("enter first number");
+const secondNumber=prompt("enter the second number");
+const accurate= confirm("firstNumber>secondNumber");
 console.log("were we accurate?",accurate);
